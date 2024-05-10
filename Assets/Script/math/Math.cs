a10e6dbf9cadb8eea356a4710cf081428046098b
hiro-okiput <130652319+hiro-okiput@users.noreply.github.com>
2024-05-10 19:25:35 +0900

    [Add] Pendulum

--- Assets/Script/math/Math.cs
+++ Assets/Script/math/Math.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 namespace GR_Game.Math
 {
-    public class Math
+    public class GR_GameMath
     {
+        public static float pi = 3.14f;
+
         public static float Sqrt(double num)
         {
             double result = num;
@@ -13,6 +18,34 @@ namespace GR_Game.Math
             {
                 result = (result * result + num) / (2 * result);
             }
+            return (float)result;
+        }
+
+        public static float DegreeToRadian(double degree) => (float)degree * (pi / 180.0f);
+
+        public static float RadianToDegree(double radian) => (float)radian * ( 180.0f / pi);
+
+        public static float CompleteEllipticIntegralOfTheFirstKind(double k, double correction)
+        {
+            double a = 1.0;
+            double b = Sqrt( 1 - k * k );
+            double sum = 1.0;
+
+            while(a - b > 1e-15)
+            {
+                double aNext = (a + b) / 2;
+                double bNext = Sqrt(a * b);
+                double amountOfChange = a - aNext;
+
+                sum -= 2 * correction * amountOfChange * amountOfChange;
+
+                correction *= 2;
+
+                a = aNext;
+                b = bNext;
+            }
+            double result = pi / (2 * a) * sum;
+
             return (float)result;
         }
     }
