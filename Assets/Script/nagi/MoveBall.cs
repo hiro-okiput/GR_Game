53485d90630e294f6bf1ec35988997b5a254c569
Radon0 <96102766+Radon0@users.noreply.github.com>
2024-10-08 17:53:57 +0900

    [update]:ボールの動きを修正
    ・角度と初速度をスライダーで設定できるように
    ・バウンドしないので次回修正

--- Assets/Script/nagi/MoveBall.cs
+++ Assets/Script/nagi/MoveBall.cs
@@ -1,38 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
 public class MoveBall : MonoBehaviour
 {
     #region 変数宣言
     ////初速
-    //private int velocity0 = 0;
-    ////角度
-    //private int theta = 0;
+    private float velocity0 = 0;
+    //角度
+    private float theta = 0;
+    //方向
+    Vector3 direction;
     ////重力
     //private int gravity = 0;
     ////時間
     //private int time = 0;
+
+    //一度だけ
+    bool firstCheck = false;
+
+    [SerializeField]
+    private Button launchButton;
+    [SerializeField]
+    private Slider thetaSlider,velocitySlider;
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        //あとで角度を設定したら自動的にVector3型になおせるようにする
-        Vector3 forceDirection = new Vector3(1.0f, 1.0f, 0f);
 
-        float forceMagnitude = 20.0f;
-
-        Vector3 force = forceMagnitude * forceDirection;
-
-        Rigidbody rb = this.gameObject.GetComponent<Rigidbody>();
-
-        rb.AddForce(force, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        theta = thetaSlider.value;
+        velocity0 = velocitySlider.value;
+    }
+
+    public void LaunchButton()
+    {
+        if (!firstCheck)
+        {
+            ChangeRadian();
+
+            Vector3 force = velocity0 * direction;
+
+            Rigidbody rb = this.gameObject.GetComponent<Rigidbody>();
+
+            rb.AddForce(force, ForceMode.Impulse);
+
+            firstCheck = true;
+        }
+    }
+
+    void ChangeRadian()
+    {
+        Debug.Log(thetaSlider.value);
+        float rad = theta * 3.14f / 180;
+
+        direction.x = Mathf.Cos(rad);
+        direction.y = Mathf.Sin(rad);
     }
 }
