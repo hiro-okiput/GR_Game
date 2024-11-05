64c75fa309289be43a2de6b635ef5b43fa897c89
Radon0 <96102766+Radon0@users.noreply.github.com>
2024-11-05 17:33:38 +0900

    [update]:車の動きを指定できるように
    ・車の向き、速さを指定することができるようになりました。
    ・二つの車を常にカメラに収めることができるようにしました。

--- Assets/Script/nagi/RelativeSpeed/CameraMove.cs
+++ Assets/Script/nagi/RelativeSpeed/CameraMove.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    public Transform redPos, bluePos;
     public Camera cam;
-    [SerializeField]
-    private GameObject blue, red;
-
-    private Vector3 bluePos, redPos,midPos;
+    private Transform camPos;
+    private Vector3 center = new Vector3(0,0,0);
+    private float radius;
+    private float margin=0.5f;
+    private float distance;
 
     // Start is called before the first frame update
     void Start()
     {
-        cam = GetComponent<Camera>();
-        bluePos = blue.transform.position;
-        redPos = red.transform.position;
-        midPos = new Vector3((redPos.x + bluePos.x) / 2, 0, (redPos.z + bluePos.z) / 2);
+        camPos = cam.gameObject.GetComponent<Transform>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        radius = 0.0f;
+        radius = Vector3.Distance(center, redPos.position);
+        radius = Mathf.Max(radius, Vector3.Distance(center, bluePos.position));
+        distance = (radius + margin) / Mathf.Sin(cam.fieldOfView * 0.5f * Mathf.Deg2Rad);
+        //Debug.Log(distance);
+        camPos.localPosition = new Vector3(0, distance, 0);
+        camPos.LookAt(this.transform);
     }
 }
--- Assets/Script/nagi/RelativeSpeed/CarMove.cs
+++ Assets/Script/nagi/RelativeSpeed/CarMove.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class CarMove : MonoBehaviour
 {
     Rigidbody rb;
+    [SerializeField]
+    private TMP_InputField inputField;
+    [SerializeField]
+    private Dropdown dropDown;
+
+    private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>(); 
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,7 +28,67 @@ public class CarMove : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 v = new Vector3(15, 0, 0);
-        rb.AddForce(v, ForceMode.Acceleration);
+        if (dropDown.value == 0)
+        {
+            if (this.name == "Blue Car")
+            {
+                Vector3 blueVector = new Vector3(speed * 5, 0, 0);
+                rb.AddForce(blueVector, ForceMode.Acceleration);
+            }
+            else if (this.name == "Red Car")
+            {
+                Vector3 redVector = new Vector3(speed * 5, 0, 0);
+                rb.AddForce(redVector, ForceMode.Acceleration);
+            }
+        }
+        if (dropDown.value == 1)
+        {
+            if (this.name == "Blue Car")
+            {
+                Vector3 blueVector = new Vector3(-speed * 5, 0, 0);
+                rb.AddForce(blueVector, ForceMode.Acceleration);
+            }
+            else if (this.name == "Red Car")
+            {
+                Vector3 redVector = new Vector3(-speed * 5, 0, 0);
+                rb.AddForce(redVector, ForceMode.Acceleration);
+            }
+        }
+        if (dropDown.value == 2)
+        {
+            if (this.name == "Blue Car")
+            {
+                Vector3 blueVector = new Vector3(0, 0, -speed * 5);
+                rb.AddForce(blueVector, ForceMode.Acceleration);
+            }
+            else if (this.name == "Red Car")
+            {
+                Vector3 redVector = new Vector3(0, 0, -speed * 5);
+                rb.AddForce(redVector, ForceMode.Acceleration);
+            }
+        }
+        if (dropDown.value == 3)
+        {
+            if (this.name == "Blue Car")
+            {
+                Vector3 blueVector = new Vector3(0, 0, speed * 5);
+                rb.AddForce(blueVector, ForceMode.Acceleration);
+            }
+            else if (this.name == "Red Car")
+            {
+                Vector3 redVector = new Vector3(0, 0, speed * 5);
+                rb.AddForce(redVector, ForceMode.Acceleration);
+            }
+        }
+    }
+
+    public void BlueSpeedGetText()
+    {
+        speed = float.Parse(inputField.text);
+    }
+
+    public void RedSpeedGetText()
+    {
+        speed = float.Parse(inputField.text);
     }
 }
