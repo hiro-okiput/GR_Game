c8177217d9ce31052938ae8e1f20ea3852e28908
Radon0 <96102766+Radon0@users.noreply.github.com>
2024-11-19 17:24:42 +0900

    [update]:矢印UIのアップデート
    ・矢印UIを追加しました。
    ・矢印はプレイヤーが入れた方向、速さによって変わります。

--- Assets/Script/nagi/RelativeSpeed/ArrowUI.cs
+++ Assets/Script/nagi/RelativeSpeed/ArrowUI.cs
@@ -11,25 +11,72 @@ public class ArrowUI : MonoBehaviour
     [SerializeField]
     private TMP_InputField inputField;
 
+    int direction = 0; //east=0,west=1,south=2,north=3
+
+    RectTransform rectTransform;
+
+    private void Start()
+    {
+        rectTransform = gameObject.GetComponent<RectTransform>();
+    }
+
     public void DirectionChoice()
     {
-        switch(dropDown.value)
+        direction = dropDown.value;
+        switch(direction)
         {
-            case 0:  //east
-                this.gameObject.transform.rotation = new Quaternion(0, 90, 0, 0);
-                break;
+            //case 0:  //east
+            //    this.gameObject.transform.rotation = new Quaternion(0, 90, 0, 0);
+            //    break;
             case 1:  //west
-                this.gameObject.transform.rotation = new Quaternion(0, -90, 0, 0);
+                rectTransform.rotation = Quaternion.Euler(0, 0, 180);
                 break;
             case 2:  //south
-                this.gameObject.transform.rotation = new Quaternion(0, 180, 0, 0);
+                rectTransform.rotation = Quaternion.Euler(0, 0, -90);
+                break;
+            case 3:  //north
+                rectTransform.rotation = Quaternion.Euler(0, 0, 90);
                 break;
         }
     }
 
     public void ScaleChoice()
     {
+        float width = 50;
+        float height = 50;
+
         float value=float.Parse(inputField.text);
-        this.gameObject.transform.localScale = new Vector3(1, 1, value);
+        if(value>=20)
+        {
+            value = 20;
+        }
+
+        Vector2 pos = rectTransform.position;
+
+        switch(direction)
+        {
+            case 0:
+                width += value * 30;
+                pos.x = width / 2 + 960;
+                pos.y = 540;
+                break;
+            case 1:
+                width += value * 30;
+                pos.x = -width / 2 + 960;
+                pos.y = 540;
+                break;
+            case 2:
+                width += value * 30;
+                pos.x = 960;
+                pos.y = -width / 2 + 540;
+                break;
+            case 3:
+                width += value * 30;
+                pos.x = 960;
+                pos.y = width / 2 + 540;
+                break;
+        }
+        rectTransform.position = pos;
+        rectTransform.sizeDelta = new Vector2(width, height);
     }
 }
