8274858a3ef12328ebb5e3eb69989a87d398c78b
hiro-okiput <130652319+hiro-okiput@users.noreply.github.com>
2025-01-25 15:00:45 +0900

    [update]

--- Assets/Script/environment/EnvironmentData.cs
+++ Assets/Script/environment/EnvironmentData.cs
@@ -5,7 +5,7 @@ namespace GR_Game
 {
     static public class EnvironmentData
     {
-        static private float gravity = 1.0f;    //d—Í
+        static private float gravity = 1f;    //d—Í
         static private TimeData currentTime;    //Œ“ú“ú
         static private float windSpeed;         //•—‘¬
         static private float windSpeedMin;      //•—‘¬_Å’á
